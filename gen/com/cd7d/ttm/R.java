fa5e9be7f3ad33ac52681152956096f997c9cca4
zhuojunxian <zhuojunxian@qq.com>
2015-03-03 13:44:28 +0800

    合并

--- gen/com/cd7d/ttm/R.java
+++ gen/com/cd7d/ttm/R.java
@@ -18,43 +18,51 @@ public final class R {
         public static final int cornerbg=0x7f020001;
         public static final int cornerwithnumber=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int tic01=0x7f020004;
-        public static final int ticno=0x7f020005;
-        public static final int ticnowithnumber=0x7f020006;
-        public static final int ticyes=0x7f020007;
+        public static final int login=0x7f020004;
+        public static final int more=0x7f020005;
+        public static final int other=0x7f020006;
+        public static final int set=0x7f020007;
+        public static final int tic01=0x7f020008;
+        public static final int ticno=0x7f020009;
+        public static final int ticnowithnumber=0x7f02000a;
+        public static final int ticyes=0x7f02000b;
     }
     public static final class id {
         public static final int button1=0x7f070006;
-        public static final int button2=0x7f070013;
-        public static final int button3=0x7f070016;
-        public static final int checkBox1=0x7f07000f;
-        public static final int contentEr=0x7f07000b;
-        public static final int datePicker1=0x7f070014;
+        public static final int button2=0x7f070015;
+        public static final int button3=0x7f070018;
+        public static final int checkBox1=0x7f070011;
+        public static final int contentEr=0x7f07000d;
+        public static final int datePicker1=0x7f070016;
         public static final int description=0x7f070001;
         public static final int jiantou=0x7f070003;
-        public static final int listView1=0x7f070017;
+        public static final int listView1=0x7f070019;
+        public static final int lv_popup_list=0x7f07000c;
         public static final int mubiao=0x7f070002;
-        public static final int myDatePicker=0x7f070012;
-        public static final int mylist=0x7f07000a;
+        public static final int myDatePicker=0x7f070014;
+        public static final int mylist=0x7f07000b;
         public static final int name=0x7f070000;
-        public static final int newtaskdesc=0x7f07000d;
-        public static final int newtaskname=0x7f07000c;
-        public static final int showdate=0x7f070010;
-        public static final int spinner1=0x7f07000e;
-        public static final int tdatetime=0x7f070011;
+        public static final int newtaskdesc=0x7f07000f;
+        public static final int newtaskname=0x7f07000e;
+        public static final int showdate=0x7f070012;
+        public static final int spinner1=0x7f070010;
+        public static final int tdatetime=0x7f070013;
         public static final int textView1=0x7f070008;
         public static final int textView2=0x7f070007;
         public static final int textView3=0x7f070009;
         public static final int tid=0x7f070005;
-        public static final int timePicker1=0x7f070015;
+        public static final int timePicker1=0x7f070017;
         public static final int tpercent=0x7f070004;
+        public static final int tv_list_item=0x7f07000a;
     }
     public static final class layout {
         public static final int itemtask=0x7f030000;
         public static final int itemwork=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int newtask=0x7f030003;
-        public static final int viewtask=0x7f030004;
+        public static final int list_item_popupwindow=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int menu=0x7f030004;
+        public static final int newtask=0x7f030005;
+        public static final int viewtask=0x7f030006;
     }
     public static final class string {
         public static final int TextView_0=0x7f05000d;
